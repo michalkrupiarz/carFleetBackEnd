6154b64801e88dfad5ce3abec77f540ee18877eb
Magdalena Fornalczyk <Magdalena Fornalczyk>
2016-12-28 22:44:24 +0100

    added way to use it from external project

--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;  
 import javax.ws.rs.Produces;  
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import service.CarService;  
 import bean.Car;
@@ -24,6 +25,7 @@ public class CarController {
 	public List <Car> getCars()
 	{	
 		List<Car> listOfCars = carService.getAllCars();
+		
 		return listOfCars;
 	}
 	@GET
